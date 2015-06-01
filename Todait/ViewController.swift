57978b1f98be21a087bf63935cc7f111919a0837
cruisediary <cruzdiary@gmail.com>
2015-06-01 12:03:44 +0900

    Initial Commit

---
+++ Todait/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 6. 1..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
