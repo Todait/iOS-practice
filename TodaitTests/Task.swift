6279495368a911309e1971a7714f5aad14489ba8
cruisediary <cruzdiary@gmail.com>
2015-06-18 17:41:42 +0900

    feat:(Test) 유닛테스트용 클래스 추가

---
+++ TodaitTests/Task.swift
@@ -0,0 +1,21 @@
+//
+//  Task.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 6. 17..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import UIKit
+
+class Task: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
