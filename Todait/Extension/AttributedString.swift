5fc6e9694f1c5dc97d5157f84985196f3e6925ff
cruisediary <cruzdiary@gmail.com>
2015-07-22 13:41:23 +0900

    feat : (Login) 로그인 UI
    #41889260399497

---
+++ Todait/Extension/AttributedString.swift
@@ -0,0 +1,13 @@
+//
+//  AttributeString.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 7. 22..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import UIKit
+
+class AttributeString: NSObject {
+   
+}
---
+++ Todait/Login/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 7. 22..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: BasicViewController {
+   
+}
