40694e26ca78a19fd00579836dc67ae1ea3505d4
cruisediary <cruzdiary@gmail.com>
2015-08-07 12:34:39 +0900

    feat : (NewTask) 시간측정 화면 키보드헬프뷰 추가
    #44603260233832

--- Todait/NewTask/TimerTaskViewController.swift
+++ Todait/NewTask/TimerTaskViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate,PeriodDelegate,CategoryDelegate{
+class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate,PeriodDelegate,CategoryDelegate,ValidationDelegate,KeyboardHelpDelegate{
     var mainColor: UIColor!
     
     var categoryButton: UIButton!
@@ -16,8 +17,8 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
     
     var taskTextField: UITextField!
     
-    var saveButton: UIButton!
-    var investButton: UIButton!
+    var completeButton: UIButton!
+
     var currentTextField: UITextField!
     
     
@@ -40,7 +41,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
     var rangeButton:UIButton!
     var unitButton:UIButton!
     
-    var option:OptionStatus = OptionStatus.everyDay
+    var option:OptionStatus = OptionStatus.None
     var isTotal:Bool! = true
     var rangeList:[[String:String]] = []
     
@@ -55,13 +56,33 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
     
     
     var closeButton:UIButton!
+    var keyboardHelpView:KeyboardHelpView
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadDefaultCategory()
         setupTimeTaskViewController()
         addTimeTaskTableView()
         
+        addKeyboardHelpView()
+        
+    }
+    
+    func loadDefaultCategory(){
+        
+        let entityDescription = NSEntityDescription.entityForName("Category",inManagedObjectContext:managedObjectContext!)
+        let request = NSFetchRequest()
+        
+        request.entity = entityDescription
+        
+        var error: NSError?
+        
+        var categoryData = managedObjectContext?.executeFetchRequest(request, error: &error) as? [Category]
+        
+        if let categoryData = categoryData {
+            category = categoryData.first
+        }
         
     }
     
@@ -73,8 +94,83 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         self.titleLabel.text = "시간 측정"
         self.titleLabel.hidden = false
         addCloseButton()
+        addCompleteButton()
+    
+        registerForKeyboardNotification()
         
     }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        resignForKeyboardNotification()
+    }
+    
+    func resignForKeyboardNotification(){
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    
+    func registerForKeyboardNotification(){
+        
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWasShown:"), name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillBeHidden:"), name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func keyboardWasShown(aNotification:NSNotification){
+        
+        var info:[NSObject:AnyObject] = aNotification.userInfo!
+        var kbSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)!.CGRectValue().size as CGSize
+        
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+            
+            self.keyboardHelpView.transform = CGAffineTransformMakeTranslation(0, -kbSize.height - 38*self.ratio)
+            
+            }) { (Bool) -> Void in
+                
+                
+        }
+        
+    }
+    
+    func keyboardWillBeHidden(aNotification:NSNotification){
+        
+        var contentInsets = UIEdgeInsetsZero
+        
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+            
+            
+            self.keyboardHelpView.transform = CGAffineTransformMakeTranslation(0,0)
+            
+            }) { (Bool) -> Void in
+                
+        }
+        
+    }
+
+    
+    func addCompleteButton(){
+        
+        if completeButton != nil {
+            return
+        }
+        
+        completeButton = UIButton(frame: CGRectMake(255*ratio, 32, 50*ratio, 20))
+        completeButton.setTitle("Done", forState: UIControlState.Normal)
+        completeButton.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 18)
+        completeButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        completeButton.addTarget(self, action: Selector("completeButtonClk"), forControlEvents: UIControlEvents.TouchUpInside)
+        completeButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Right
+        view.addSubview(completeButton)
+        
+    }
+    
     
     func addCloseButton(){
         
@@ -82,8 +178,8 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
             return
         }
         
-        closeButton = UIButton(frame: CGRectMake(10, 30, 28, 28))
-        closeButton.setBackgroundImage(UIImage(named: "newgoal_closed@3x.png"), forState: UIControlState.Normal)
+        closeButton = UIButton(frame: CGRectMake(2, 22, 44, 44))
+        closeButton.setBackgroundImage(UIImage(named: "nav_bt_closed@3x.png"), forState: UIControlState.Normal)
         closeButton.addTarget(self, action: Selector("closeButtonClk"), forControlEvents: UIControlEvents.TouchUpInside)
         view.addSubview(closeButton)
     }
@@ -94,6 +190,111 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
             
         })
     }
+    
+    func completeButtonClk(){
+        
+        
+        let validator = Validator()
+        
+        
+        validator.registerField(taskTextField, rules:[MinLengthRule(length: 1, message: "목표를 입력해주세요.")])
+        validator.validate(self)
+        
+    }
+    
+    func validationSuccessful(){
+        saveNewTask()
+    }
+    
+    func validationFailed(errors: [UITextField:ValidationError]){
+        
+        
+        for ( textField , error) in errors {
+            
+            let alert = UIAlertView(title: "Invalid", message: error.errorMessage, delegate: nil, cancelButtonTitle: "Cancel")
+            
+            alert.show()
+            
+            return
+        }
+    }
+    
+    
+    
+    
+    
+    func saveNewTask(){
+        
+        let entityDescription = NSEntityDescription.entityForName("Task", inManagedObjectContext:managedObjectContext!)
+        let task = Task(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext)
+        
+        task.name = taskTextField.text
+        task.unit = ""
+        
+        
+        let dateForm = NSDateFormatter()
+        dateForm.dateFormat = "yyyyMMdd"
+        
+        task.startDate = getDateNumberFromDate(periodStartDate)
+        task.endDate = getDateNumberFromDate(periodEndDate)
+        //task.unit = "개" //unitTextField.text
+        //task.categoryId = category
+        task.categoryId = category
+        task.taskType = "Timer"
+        
+        saveNewWeek(task)
+        
+        var error: NSError?
+        managedObjectContext?.save(&error)
+        
+        if let err = error {
+            //에러처리
+        }else{
+            
+            NSLog("Task 저장성공",1)
+            registerAlarm()
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+    }
+    
+    
+    func saveNewWeek(task:Task){
+        
+        let entityDescription = NSEntityDescription.entityForName("Week", inManagedObjectContext:managedObjectContext!)
+        let week = Week(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext)
+        week.taskId = task
+        
+        
+        week.sun = 3600//investData[0];
+        week.mon = 3600//investData[1];
+        week.tue = 3600//investData[2];
+        week.wed = 3600//investData[3];
+        week.thu = 3600//investData[4];
+        week.fri = 3600//investData[5];
+        week.sat = 3600//investData[6];
+        
+        
+        var error: NSError?
+        managedObjectContext?.save(&error)
+        
+        
+    }
+    
+    func registerAlarm(){
+        
+        
+        let notification = UILocalNotification()
+        notification.alertBody = taskTextField.text
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        notification.fireDate = NSDate().dateByAddingTimeInterval(5)
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.hasAction = true
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
+    }
+
+    
     
     func setupTimeTaskViewController(){
         startDate = NSDate()
@@ -119,6 +320,39 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         timeTaskTableView.bounces = false
         timeTaskTableView.separatorStyle = UITableViewCellSeparatorStyle.None
         view.addSubview(timeTaskTableView)
+    }
+    
+    
+    func addKeyboardHelpView(){
+        
+        keyboardHelpView = KeyboardHelpView(frame: CGRectMake(0, height , width, 38*ratio + 185*ratio))
+        keyboardHelpView.backgroundColor = UIColor.whiteColor()
+        keyboardHelpView.delegate = self
+        keyboardHelpView.setStatus(KeyboardHelpStatus.Center)
+        
+        view.addSubview(keyboardHelpView)
+        
+        
+        let line = UIView(frame: CGRectMake(0, 38*ratio-1, width, 1))
+        line.backgroundColor = UIColor.colorWithHexString("#d1d5da")
+        keyboardHelpView.addSubview(line)
+        
+    }
+    
+    
+    func confirmButtonClk(){
+        
+        currentTextField.textColor = UIColor.todaitGray()
+        currentTextField.resignFirstResponder()
+        
+    }
+    
+    func leftButtonClk() {
+        return
+    }
+    
+    func rightButtonClk() {
+        return
     }
     
     
@@ -237,7 +471,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         taskTextField.textAlignment = NSTextAlignment.Left
         taskTextField.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14*ratio)
         taskTextField.textColor = UIColor.colorWithHexString("#969696")
-        taskTextField.returnKeyType = UIReturnKeyType.Next
+        taskTextField.returnKeyType = UIReturnKeyType.Done
         taskTextField.backgroundColor = UIColor.whiteColor()
         taskTextField.addTarget(self, action: Selector("updateAllEvents:"), forControlEvents: UIControlEvents.AllEvents)
         taskTextField.text = aimString
@@ -292,6 +526,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         categoryButton.addTarget(self, action: Selector("showCategorySettingVC"), forControlEvents: UIControlEvents.TouchUpInside)
         cell.contentView.addSubview(categoryButton)
         
+        categoryEdited(category)
     }
     
     func showCategorySettingVC(){
@@ -313,8 +548,10 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         
         categoryButton.layer.borderColor = UIColor.clearColor().CGColor
         categoryButton.setImage(UIImage.maskColor("category@3x.png", color: UIColor.whiteColor()), forState: UIControlState.Normal)
-        categoryButton.backgroundColor = UIColor.colorWithHexString(editedCategory.color)
-     
+        
+        categoryButton.setBackgroundImage(UIImage.maskColor("circle@3x.png", color: UIColor.colorWithHexString(editedCategory.color)), forState: UIControlState.Normal)
+        categoryButton.setBackgroundImage(UIImage.maskColor("circle@3x.png", color: UIColor.todaitLightGray()), forState: UIControlState.Highlighted)
+        
         self.category = editedCategory
     }
     
@@ -548,7 +785,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         titleLabel.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 12.5*ratio)
         dayOption.addSubview(titleLabel)
         
-        
+        /*
         if option == OptionStatus.everyDay {
             iconImageView.image = UIImage(named: "icon_week_wt@3x.png")
             titleLabel.textColor = UIColor.todaitGreen()
@@ -556,12 +793,12 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
             iconImageView.image = UIImage(named: "icon_week@3x.png")
             titleLabel.textColor = UIColor.todaitGray()
         }
-        
+        */
     }
     
     func dayOptionClk(){
         
-        option = OptionStatus.everyDay
+        //option = OptionStatus.everyDay
         timeTaskTableView.reloadData()
         
     }
@@ -586,7 +823,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         
         
         
-        if option == OptionStatus.alarm {
+        if option == OptionStatus.Alarm {
             iconImageView.image = UIImage(named: "icon_alarm_wt@3x.png")
             titleLabel.textColor = UIColor.todaitGreen()
         }else{
@@ -600,7 +837,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         
         showAlarmVC()
         
-        option = OptionStatus.alarm
+        option = OptionStatus.Alarm
         timeTaskTableView.reloadData()
         
     }
@@ -635,7 +872,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         reviewOption.addSubview(titleLabel)
         
         
-        if option == OptionStatus.review {
+        if option == OptionStatus.Review {
             iconImageView.image = UIImage(named: "icon_review_wt@3x.png")
             titleLabel.textColor = UIColor.todaitGreen()
         }else{
@@ -646,7 +883,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
     
     func reviewOptionClk(){
         
-        option = OptionStatus.review
+        option = OptionStatus.Review
         timeTaskTableView.reloadData()
         
     }
@@ -669,7 +906,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
         reReadOption.addSubview(titleLabel)
         
         
-        if option == OptionStatus.reRead {
+        if option == OptionStatus.Reread {
             iconImageView.image = UIImage(named: "icon_reread_wt@3x.png")
             titleLabel.textColor = UIColor.todaitGreen()
         }else{
@@ -681,7 +918,7 @@ class TimerTaskViewController: BasicViewController,UITableViewDelegate,UITableVi
     
     func reReadOptionClk(){
         
-        option = OptionStatus.reRead
+        option = OptionStatus.Reread
         timeTaskTableView.reloadData()
         
     }
