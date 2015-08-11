ae6fd6f974359a7d4e8009b10240463c49310846
cruisediary <cruzdiary@gmail.com>
2015-08-11 11:42:23 +0900

    feat (View) Nib파일로부터 View 가져오기 추가

--- Todait/Extension/Utill.swift
+++ Todait/Extension/Utill.swift
@@ -15,6 +15,33 @@ public let CREDENTIAL_SECRET_KEY:String = "AisL25f74Aaf5N5XpkyoTuOAc6ccw0TGDwPDx
 public let AWSS3_BUCKET_NAME:String = "todait-images"
 
 
+
+extension UIView {
+    class func fromNib<T : UIView>(nibNameOrNil: String? = nil) -> T {
+        let v: T? = fromNib(nibNameOrNil: nibNameOrNil)
+        return v!
+    }
+    
+    class func fromNib<T : UIView>(nibNameOrNil: String? = nil) -> T? {
+        var view: T?
+        let name: String
+        if let nibName = nibNameOrNil {
+            name = nibName
+        } else {
+            // Most nibs are demangled by practice, if not, just declare string explicitly
+            name = "\(T.self)".componentsSeparatedByString(".").last!
+        }
+        let nibViews = NSBundle.mainBundle().loadNibNamed(name, owner: nil, options: nil)
+        for v in nibViews {
+            if let tog = v as? T {
+                view = tog
+            }
+        }
+        return view
+    }
+}
+
+
 public func getIndexFromCategoryColorString(color:String)->Int{
     
     switch color{
