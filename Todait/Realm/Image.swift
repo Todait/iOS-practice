5ad96ce2053ab7fd02bdc14de9c1f23a5a0460a3
cruisediary <cruzdiary@gmail.com>
2015-08-13 14:49:14 +0900

    delete (Delete) 코어데이터 모델, Old파일 제거

---
+++ Todait/Realm/Image.swift
@@ -0,0 +1,18 @@
+//
+//  Image.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 8. 13..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import RealmSwift
+
+class Image: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
