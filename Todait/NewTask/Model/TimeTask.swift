f6018ba20678d60329ddc62b4de65e0bebfa4f09
cruisediary <cruzdiary@gmail.com>
2015-08-14 20:38:29 +0900

    feat : (Realm + NewTask) 일정추가 모델 추가

---
+++ Todait/NewTask/Model/TimeTask.swift
@@ -0,0 +1,13 @@
+//
+//  TimeTask.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 8. 14..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import UIKit
+
+class TimeTask: NSObject {
+   
+}
---
+++ Todait/NewTask/NewGoalStep4TimeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewGoalStep4TimeViewController.swift
+//  Todait
+//
+//  Created by CruzDiary on 2015. 8. 14..
+//  Copyright (c) 2015년 GpleLab. All rights reserved.
+//
+
+import UIKit
+
+class NewGoalStep4TimeViewController: NewGoalStep4ViewController {
+   
+}
